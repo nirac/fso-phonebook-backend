32d19f681caa871db47746a0684c7db7b63b5250
Carin Ivarsson <caiv1096@gmail.com>
2023-01-19 14:20:08 +0000

    completed up to 3.6

--- index.js
+++ index.js
@@ -24,14 +24,76 @@ let persons = [
       "number": "39-23-6423122"
     }
 ];
-
+app.use(express.json());
 app.get('/', (request,response)=>{
     response.send('<h1>Phonebook</h1>')
+});
+
+app.get('/info', (request, response)=>{
+    const entries = persons.length;
+    const date = new Date();
+    response.send(`<p>Phonebook has info for ${entries} people</p><p>${date.toString()}</p>`)
 });
 
 app.get('/api/persons', (request, response)=>{
     response.json(persons);
 });
+
+app.get('/api/persons/:id', (request, response)=>{
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id===id);
+
+    if(person) {
+        response.json(person);
+    }else{
+        response.status(404).end();
+    }
+})
+
+const generateId = () => {
+    let newId = Math.floor(Math.random()*10000);
+    const checkId = persons.includes(newId) ? generateId():newId;
+    console.log(newId)
+    return newId;
+};
+
+app.post('/api/persons', (request, response) =>{
+    const body = request.body;
+    console.log(22, request.headers);
+    console.log(33, body)
+    if(!body.name) {
+        return response.status(400).json({
+            error:'name is missing'
+        })
+    }else if(!body.number){
+        return response.status(400).json({
+            error:'number is missing'
+        })
+    }else if(persons.some(p => p.name === body.name)){
+        return response.status(400).json({
+            error:'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person);
+
+    response.json(person)
+})
+
+
+
+app.delete('/api/persons/:id', (request, response)=> {
+    const id= Number(request.params.id);
+    persons = persons.filter(person => person.id!= id);
+
+    response.status(204).end();
+})
 
 const PORT = 3001;
 
